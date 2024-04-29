a7ee17fea4393bc00906a18682093ba4cf19dd3e
“Vince <vincenzo.reo@passportinc.com>
2024-04-29 09:31:32 -0400

    initial commit for weather applicaation

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Starting app...');
+
+setTimeout(() => {
+  console.log('two second timer...');
+}, 2000);
+
+setTimeout(() => {
+  console.log('zero second timer...');
+}, 0);
+
+
+console.log('Stopping app...');
