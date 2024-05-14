3f65041ccb88118ffe333f49c433dcc7c6741a79
“Vince <vincenzo.reo@passportinc.com>
2024-05-14 14:38:22 -0400

    add forecast utils

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const request = require('postman-request');
+const forecast = require('./utils/forecast');
 const geocode = require('./utils/geocode');
 
-// const url = '';
 
-// request({ 
-//   url: url,
-//   json: true,
-// }, 
-// (error, response) => {
-//   if (error) {
-//     console.log(`Unable to connect to weather service.`);
-//   } else if (response.body.error) {
-//     console.log(`Unable to find location.`);
-//   } else {
-//     const weatherDescription = response.body.current.weather_descriptions[0];
-//     const currentTemp = response.body.current.temperature;
-//     const feelsLikeTemp = response.body.current.feelslike;
-
-//     console.log(`${weatherDescription}. It is currently ${currentTemp} fahrenheit out. It feels like ${feelsLikeTemp} fahrenheit out.`)
-//   }
-// });
-
+forecast('Charlotte', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data);
+});
 
 geocode('Charlotte', (error, data) => {
   console.log('Error', error);
--- utils/geocode.js
+++ utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const geocode = (address, callback) => {
-  const access_token = 'pk.eyJ1IjoidmpyZW85MSIsImEiOiJjbHc2b3Nuc3cyMW1sMmlucjBqdmRyZ3NhIn0.eda5GqvO5ujgC1Ho3UydHw'
+  const access_token = ''
   const url = `https://api.mapbox.com/search/geocode/v6/forward?q=${address}&access_token=${access_token}`;
 
   request({ url: url, json: true }, (error, response) => {
